90fb0d1b4db28cbe3610edaf7d77e3e61c1f59c9
dhuayta <denny_ha@hotmail.com>
2012-09-09 23:51:31 -0500

    convert 20 to XX

--- code_specs.rb
+++ code_specs.rb
@@ -1,1 +1,12 @@
-test
+require 'minitest/autorun'
+require_relative 'roman'
+
+describe Roman do
+  before do
+    @roman = Roman.new
+  end
+  it 'should convert 20 to XX' do    
+    @roman.convert(20).must_equal "XX" #assert
+  end
+
+end

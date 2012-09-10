6a1f17a3d2e92f1a606d3998fd36b9a707c6298f
dhuayta <denny_ha@hotmail.com>
2012-09-10 00:05:54 -0500

    convert 1

--- code_specs.rb
+++ code_specs.rb
@@ -62,5 +62,8 @@ describe Roman do
   it 'should convert 2 to II' do    
     @roman.convert(2).must_equal "II" #assert
   end
+  it 'should convert 1 to I' do    
+    @roman.convert(1).must_equal "I" #assert
+  end
 end
 
